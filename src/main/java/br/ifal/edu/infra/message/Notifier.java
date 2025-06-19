f1eed632b3ec8b3171d3efc39cc90b49e166d6a5
Italo1033 <italogammy1033@gmail.com>
2025-06-18 23:14:08 -0300

    Refactor Notifier to include queue name in sendMessage method and improve resource management

--- src/main/java/br/ifal/edu/infra/message/Notifier.java
+++ src/main/java/br/ifal/edu/infra/message/Notifier.java
@@ -1,35 +1,25 @@
 package br.ifal.edu.infra.message;
 
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.*;
 
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
 
 public class Notifier implements INotifier {
 
-
     @Override
-    public void sendMessage(String message) {
+    public void sendMessage(String message, String queueName) {
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
         factory.setUsername("guest");
         factory.setPassword("guest");
 
-        try {
-            Connection connection = factory.newConnection();
-            Channel channel = connection.createChannel();
-            System.out.println("Enviando mensagem .. ");
-            channel.basicPublish("", "send-emails", null, message.getBytes("UTF-8"));
-
-            channel.close();
-            connection.close();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        } catch (TimeoutException e) {
+        try (Connection connection = factory.newConnection(); Channel channel = connection.createChannel()) {
+            channel.queueDeclare(queueName, false, false, false, null);
+            System.out.println("Enviando mensagem para [" + queueName + "]...");
+            channel.basicPublish("", queueName, null, message.getBytes("UTF-8"));
+        } catch (IOException | TimeoutException e) {
             throw new RuntimeException(e);
         }
-
     }
 }
